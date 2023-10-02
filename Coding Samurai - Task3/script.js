cd33d622cd00c966f89f11bc6c0641c5f2085d1f
Rajat2401 <144329916+Rajat2401@users.noreply.github.com>
2023-10-02 21:09:04 +0530

    Add files via upload

---
+++ Coding Samurai - Task3/script.js
@@ -0,0 +1,23 @@
+let input = document.getElementById('input')
+let button = document.querySelectorAll('button')
+button.forEach(Element => {
+    Element.addEventListener('click', (e) => {
+        console.log(e.target.textContent)
+        if (e.target.textContent === "C") {
+            input.innerHTML = ""
+        }
+        
+        else if (e.target.textContent === "⌫") {
+            input.innerHTML = input.innerHTML.slice(0, -1)
+        }
+        else if (e.target.textContent === "=") {
+            input.innerHTML = eval(input.innerHTML)
+        }
+
+        else {
+            input.innerHTML += e.target.textContent
+        }
+        input.scrollLeft = input.scrollWidth
+    })
+
+})
